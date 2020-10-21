14f78b63d9f512081366d2ba86c1fcf977d29af9
OleksandrYehorov <egorow.sasha@gmail.com>
2020-10-22 00:22:50 +0300

    Updates

---
+++ precache-manifest.12fc4223ad078eb4714f4eea2aa15bef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc40c5ba8f02fea4b6dc0a24d46ef4a8",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "a158864126dd1c3847a2",
+    "url": "/minesweeper/static/js/2.13537296.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/minesweeper/static/js/2.13537296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4761023eb71df8c07e",
+    "url": "/minesweeper/static/js/main.5d192ee3.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  },
+  {
+    "revision": "4ecf993fb87b7c9e7e2786cd5d720482",
+    "url": "/minesweeper/static/media/DSEG7Classic-Bold.4ecf993f.woff2"
+  },
+  {
+    "revision": "e700831dee1b10a94c5c16fdbf4458e8",
+    "url": "/minesweeper/static/media/DSEG7Classic-Bold.e700831d.ttf"
+  },
+  {
+    "revision": "f4621db909a2a26a680209f99fa0874e",
+    "url": "/minesweeper/static/media/DSEG7Classic-Bold.f4621db9.woff"
+  },
+  {
+    "revision": "1fc76a19153fae3bb94fbb274959fd6b",
+    "url": "/minesweeper/static/media/crossedMine.1fc76a19.svg"
+  },
+  {
+    "revision": "9750a8d1476dd9d417439d74b17900d2",
+    "url": "/minesweeper/static/media/flag.9750a8d1.svg"
+  },
+  {
+    "revision": "b0c0cb285c29fef089b29e8c6a9de53e",
+    "url": "/minesweeper/static/media/mine.b0c0cb28.svg"
+  }
+]);
