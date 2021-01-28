0f6120a623a69b879673dcc26e9e7787d0899b04
OleksandrYehorov <egorow.sasha@gmail.com>
2021-01-29 01:33:52 +0200

    Refactor type declarations

---
+++ src/@types/types.d.ts
@@ -0,0 +1,14 @@
+declare module '*.woff2' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.woff' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ttf' {
+  const src: string;
+  export default src;
+}
