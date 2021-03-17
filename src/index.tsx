7cf183aefaabf86e179df0cf54886ccf9c7f369f
OleksandrYehorov <egorow.sasha@gmail.com>
2021-03-17 17:27:07 +0200

    Fix analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import { render } from 'react-dom';
 import { App } from './App';
 import { register as registerServiceWorker } from './serviceWorkerRegistration';
-import { initFirebase } from './services/firebase';
 
 render(<App />, document.getElementById('root'));
 
-initFirebase();
 registerServiceWorker();
--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -3,8 +3,27 @@ import firebase from 'firebase';
 import { Difficulty } from '../utils/constants';
 import { now } from '../utils/now';
 
-const firebaseAnalytics =
-  process.env.NODE_ENV === 'production' ? firebase.analytics() : null;
+const firebaseConfig = {
+  apiKey: 'AIzaSyDJdOEpo7tADYFOhGZwyHJrKQ66tLUZ_Sk',
+  authDomain: 'minesweeper-12668.firebaseapp.com',
+  projectId: 'minesweeper-12668',
+  storageBucket: 'minesweeper-12668.appspot.com',
+  messagingSenderId: '227670634951',
+  appId: '1:227670634951:web:e4cd4f9c038577b7c369e2',
+  measurementId: 'G-BT2Q977QMC',
+};
+
+const initFirebaseAnalytics = () => {
+  if (process.env.NODE_ENV === 'production') {
+    firebase.initializeApp(firebaseConfig);
+
+    return firebase.analytics();
+  }
+
+  return null;
+};
+
+const firebaseAnalytics = initFirebaseAnalytics();
 
 export type AnalyticsEvents = {
   start_game: {
