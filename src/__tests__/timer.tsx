266daffb9f4e78c54bfc59cb997b54c6dd03bce5
OleksandrYehorov <egorow.sasha@gmail.com>
2021-08-20 20:59:28 +0300

    Fix "act" warnings in tests

--- src/__tests__/timer.tsx
+++ src/__tests__/timer.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { App } from '../App';
 import { getCell } from '../test-utils/cellQueries';
@@ -10,12 +10,16 @@ beforeEach(() => render(<App />));
 
 test('timer shows playing time', () => {
   expect(screen.getByLabelText(/timer/i)).toHaveTextContent('0');
-  jest.advanceTimersByTime(3100);
+  act(() => {
+    jest.advanceTimersByTime(3100);
+  });
   // must be still 0 because game is not started
   expect(screen.getByLabelText(/timer/i)).toHaveTextContent('0');
   userEvent.click(getCell(minesMockData.beginner.firstClick));
   expect(screen.getByLabelText(/timer/i)).toHaveTextContent('0');
-  jest.advanceTimersByTime(3000);
+  act(() => {
+    jest.advanceTimersByTime(3000);
+  });
   // must be 3 since game was started 3 seconds ago
   expect(screen.getByLabelText(/timer/i)).toHaveTextContent('3');
   userEvent.click(screen.getByRole('button', { name: /restart/i }));
